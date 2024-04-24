49cba136317efa3309d1c8c8908473093636d49c
Lucas Heil <lightnight7615@gmail.com>
2024-04-23 19:39:48 -0600

    Prep Assignment 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -3,7 +3,71 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
     {
-        Console.WriteLine("Hello Prep2 World!");
+        
+        Console.WriteLine("Please Enter your Grade: ");
+        float Grade = float.Parse(Console.ReadLine());
+        string letter = "";
+
+        if (Grade >= 93.00)
+        {
+            letter = "A";
+        }
+        else if (Grade >= 90.00)
+        {
+            letter = "A-";
+        }
+          else if (Grade >= 87.00)
+        {
+            letter = "B+";
+        }
+          else if (Grade >= 83.00)
+        {
+            letter = "B";
+        }
+          else if (Grade >= 80.00)
+        {
+            letter = "B-";
+        }
+          else if (Grade >= 77.00)
+        {
+            letter = "C+";
+        }
+          else if (Grade >= 73.00)
+        {
+            letter = "C";
+        }
+          else if (Grade >= 70)
+        {
+            letter = "C-";
+        }
+           else if (Grade >= 67)
+        {
+            letter = "D+";
+        }
+           else if (Grade >= 63)
+        {
+            letter = "D";
+        }
+           else if (Grade >= 60)
+        {
+            letter = "D-";
+        }
+          else
+        {
+            letter = "F";
+        }
+
+        if (Grade >= 70)
+        {
+            Console.WriteLine($"Congrats you passed with a grade of {letter}. ");
+        }
+        else
+        {
+            Console.WriteLine("You didn't pass this time but you only fail if you stop trying.");
+        }
+
+
     }
 }
