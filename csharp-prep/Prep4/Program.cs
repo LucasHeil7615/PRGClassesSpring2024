3d10a20f88aaef77645a714c1774b02a9c76150d
Lucas Heil <lightnight7615@gmail.com>
2024-04-29 19:14:25 -0600

    writeLIne --> write

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -11,14 +11,18 @@ class Program
         List<int> numbers = new List<int>();
 
         Console.WriteLine("We have created a list that uses ints for you. please begin entering ints. When you would like to stop enter 0. ");
-
-        int entry = int.Parse(Console.ReadLine());
+        int entry = 1;
+        while (entry != 0){
+        Console.Write("Enter an int: ");
+        entry = int.Parse(Console.ReadLine());
         
-        while (entry != 0)
+        if (entry != 0)
         {
             numbers.Add(entry);
         }
+        }
 
+    
         Console.WriteLine("list of ints: ");
         foreach (int number in numbers)
         {
