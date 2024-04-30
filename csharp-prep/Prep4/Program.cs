f9a5101370d078890568c195ef59a5c3528bf3f6
Lucas Heil <lightnight7615@gmail.com>
2024-04-29 19:06:59 -0600

    Continued progress on prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,15 +1,28 @@
-using System;
+
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 class Program
 {
     static void Main(string[] args)
     {
-        List<string> words = new List<string>();
 
-        words.Add("Pizza");
-        words.Add("mtdew");
+        Console.WriteLine("Welcome to the list builder!. ");
 
-        Console.WriteLine($"{words}");
+        List<int> numbers = new List<int>();
 
+        Console.WriteLine("We have created a list that uses ints for you. please begin entering ints. When you would like to stop enter 0. ");
+
+        int entry = int.Parse(Console.ReadLine());
+        
+        while (entry != 0)
+        {
+            numbers.Add(entry);
+        }
+
+        Console.WriteLine("list of ints: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine($"{number}");
+        }
     }
 }
