dff120c39e153b06c9766701f31af8375a3cf916
Lucas Heil <lightnight7615@gmail.com>
2024-04-30 19:01:48 -0600

    Prep 5 first attempt

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        string name = GetName();
+        int favNum = GetInt(); 
+        int squaredNum = SquareInt(favNum); 
+        DisplayResult(name, squaredNum); 
+    }
+    
+    static void DisplayMessage()
+    {
+        Console.WriteLine("Welcome to the Program! :)");
+    }
+
+    static string GetName()
+    {
+        DisplayMessage(); // Call the display message here
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int GetInt()
+    {
+        Console.WriteLine("Please enter your favorite number: ");
+        int favNum = int.Parse(Console.ReadLine());
+        return favNum;
+    }
+
+    static int SquareInt(int favNum)
+    {
+        int squaredNum = favNum * favNum;
+        return squaredNum;
+    }
+
+    static void DisplayResult(string name, int squaredNum)
+    {
+        Console.WriteLine($"Hello {name}, the square of your number is {squaredNum}");
     }
 }
