d62d7db972d058c1f1c6a1fcb95452fbd53bb139
Lucas Heil <lightnight7615@gmail.com>
2024-05-15 16:00:46 -0600

    Journal app update

--- prove/Develop02/Journal.cs
+++ prove/Develop02/Journal.cs
@@ -1,47 +1,29 @@
 using System;
-using System.ComponentModel.Design;
-using System.Data;
-using System.Net.Quic;
-using System.Xml.Serialization;
+using System.Dynamic;
 
-class Journal
+class Compiler 
 {
-    public void Menu()
+    private static void Load()
     {
-        Console.WriteLine("Welcome to the Journal app. ");
 
-        int choice = 8;
-        while (choice != 0 )
-        {
-            Console.WriteLine("Please select an option. ");
-            Console.WriteLine("1 Write");
-            Console.WriteLine("2 Save");
-            Console.WriteLine("3 Display");
-            Console.WriteLine("4 Load");
-            Console.WriteLine("5 Inpiration");
-            Console.WriteLine("0 Quit");
-            choice = int.Parse(Console.ReadLine());
-
-            switch (choice)
-            {
-                case 1:
-                break;
-
-                case 2:
-                break;
-
-                case 3:
-                break;
-
-                case 4:
-                break;
-
-                case 5:
-                break;
-
-                case 0:
-                break;
-            }
-        }
     }
+
+    private static void save()
+    {
+        
+    }
+    private static void Display()
+    {
+
+    }
+
+    public void GetInspiration(string Quote)
+    {
+        Fillings fillings = new Fillings();
+
+        string inspired = fillings.Inspiration();
+
+        Console.WriteLine($"{Quote}");
+
+    } 
 }
