29be46952e3ce3172a5a570a322beccdb70eefcd
Lucas Heil <lightnight7615@gmail.com>
2024-06-06 16:01:06 -0600

    Load Scripture and get Scripture finalized

--- prove/Develop03/LoadScripture.cs
+++ prove/Develop03/LoadScripture.cs
@@ -1,64 +1,66 @@
 using System;
 using System.Reflection.Metadata;
+using System.IO;
 class LoadScripture
 {
 
     private string fulltext;
     private string refrence;
 
-    private int check = 1;
+    
     public void Run()
     {
         Load();
-        Getfulltext();
         GetRefrence();
+        Getfulltext();
     }
     private void Load()
     {
         //Prompt the user for a filename and then load the refrence with it's accompaning scripture from that file. This should replace any entries currently stored the memorizer.
-        while(check != 0)
+        bool fileloaded = false;
+
+        while(!fileloaded)
         {
         Console.Write("Please enter the filename you would like to enter: ");
         string filename = Console.ReadLine();
 
         if (!File.Exists(filename))
         {
-            Console.WriteLine("File not Found.");
-            break;
+            Console.WriteLine("File not Found. If you have not set any local txt files please restart program and select Get scripture. Otherwise, please retry and check spelling.");
+            continue;
         }
+        Console.WriteLine($"{filename} found");
         
         //Clearing existing entries 
         //memorizer.Clear();
         Console.Write("Please enter the refrence of the scripture that you want to load: ");
-        string refrence = Console.ReadLine();
+        string inputRefrence = Console.ReadLine();
         // read all lines from the file 
         string[] lines = File.ReadAllLines(filename);
 
         foreach (string line in lines )
         { 
-               if(line.Substring(0,refrence.Count()) == refrence)
-               {
-                 string [] parts = line.Split("|");
-                refrence = parts[0];
-                fulltext = parts[1];
-                check = 1;
-                Console.WriteLine($"{refrence} has loaded from {filename}");
-               }
-
-               else
+            string [] parts = line.Split("|");
+            if(parts.Length >=2 && parts[0].Equals(inputRefrence, StringComparison.OrdinalIgnoreCase))
+            {
+            refrence = parts[0];
+            fulltext = parts[1];
+            Console.WriteLine($"{refrence} has loaded from {filename}");
+            fileloaded = true;
+            break;
+            }
+        }
+           if (!fileloaded)
                {
                     Console.WriteLine("There doesn't seem to be a refrence here that matches your's\nPlease try again.");
-                    break;
+                    continue;
                }
         }
-        }
-        
-
     }
 
     public string GetRefrence()
     {
-        Console.Write($"{refrence}");
+        Console.Write($"{refrence}  ");
         return refrence;
     }
 
